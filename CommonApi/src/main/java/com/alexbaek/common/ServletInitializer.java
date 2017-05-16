a19c3e1c02fd982e7aff28e0e8c2cf7a42401a20
alexbaek <alexbaek31@gmail.com>
2017-05-16 10:12:47 +0900

    프로젝트 생성.

---
+++ CommonApi/src/main/java/com/alexbaek/common/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.alexbaek.common;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommonApiApplication.class);
+	}
+
+}
