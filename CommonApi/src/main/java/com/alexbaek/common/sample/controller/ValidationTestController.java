f7f53707d825d7cd535faead0a2ec7eb2e94ad82
alexbaek <alexbaek31@gmail.com>
2017-05-22 09:52:55 +0900

    소스 정리중.

---
+++ CommonApi/src/main/java/com/alexbaek/common/sample/controller/ValidationTestController.java
@@ -0,0 +1,21 @@
+/***************************************************************************************************
+*   @클래스명  : ValidationTestController.java
+*   @패키지명  : com.alexbaek.common.sample.controller
+*   @작성자	: 백우영
+*   @작성일	: 2017. 5. 22. 오전 9:41:02
+************************** @수정이력 ****************************************************************
+*  수정일    수정자   수정내용
+*     
+***************************************************************************************************/
+package com.alexbaek.common.sample.controller;
+
+/**
+ * The Class ValidationTestController.java.
+ * 설명 :
+ *
+ * @author 백우영
+ * @version 1.0
+ */
+public class ValidationTestController {
+
+}
